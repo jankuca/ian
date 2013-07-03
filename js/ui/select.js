b24c02c496b5e2a6a7660b31a17231041fa0666e
jankuca <jan@jankuca.com>
2013-07-03 19:24:39 +0200

    ian.ui.Select has a JS-based dropdown

--- js/ui/select.js
+++ js/ui/select.js
@@ -6,6 +6,7 @@ goog.require('goog.dom.forms');
 goog.require('goog.events.Event');
 goog.require('goog.events.KeyCodes');
 goog.require('goog.events.KeyHandler');
+goog.require('goog.style');
 goog.require('goog.ui.Component');
 
 
@@ -18,10 +19,14 @@ ian.ui.Select = function () {
 
   this.select_box_ = null;
   this.value_el_ = null;
+  this.dropdown_items_ = [];
+  this.is_dropdown_disabled_ = false;
 
   this.key_handler_ = null;
 
   this.value_ = '';
+  this.is_open_ = false;
+  this.selected_index_ = 0;
 };
 
 goog.inherits(ian.ui.Select, goog.ui.Component);
@@ -44,16 +49,46 @@ ian.ui.Select.convertSelectBoxesInElement = function (container) {
 };
 
 
+ian.ui.Select.prototype.setDropdownDisabled = function (disable) {
+  this.is_dropdown_disabled_ = !!disable;
+};
+
+
 ian.ui.Select.prototype.decorate = function (select_box) {
   this.select_box_ = select_box;
 
   var dom = this.getDomHelper();
   var el = dom.createDom('div', { 'class': 'select' });
-  this.value_el_ = dom.createDom('span', { 'class': 'select-value' });
+  el.setAttribute('tabindex', '-1');
 
+  this.value_el_ = dom.createDom('span', { 'class': 'select-value' });
   el.appendChild(this.value_el_);
+
+  if (!goog.userAgent.MOBILE && !this.is_dropdown_disabled_) {
+    this.dropdown_el_ = dom.createDom('ul', { 'class': 'select-options' });
+
+    var dropdown_items = [];
+    this.dropdown_items_ = dropdown_items;
+
+    for (var i = 1, ii = select_box.options.length; i < ii; ++i) {
+      var option = select_box.options[i];
+      var dropdown_item_label = dom.getTextContent(option) || option.value;
+      var dropdown_item_el = dom.createDom('li', {}, dropdown_item_label);
+      if (i === select_box.selectedIndex) {
+        goog.dom.classes.add(dropdown_item_el, 'selected');
+      }
+      this.dropdown_el_.appendChild(dropdown_item_el);
+      dropdown_items.push(dropdown_item_el);
+    }
+
+    goog.style.setElementShown(this.dropdown_el_, false);
+    el.appendChild(this.dropdown_el_);
+  }
+
   select_box.parentNode.insertBefore(el, this.select_box_);
   el.appendChild(this.select_box_);
+
+  goog.style.setElementShown(this.select_box_, false);
 
   goog.base(this, 'decorate', el);
 
@@ -64,20 +99,27 @@ ian.ui.Select.prototype.decorate = function (select_box) {
 ian.ui.Select.prototype.enterDocument = function () {
   goog.base(this, 'enterDocument');
 
-  this.key_handler_ = new goog.events.KeyHandler(this.select_box_);
-
+  var el = this.getElement();
   var handler = this.getHandler();
+  this.key_handler_ = new goog.events.KeyHandler(el);
+
   handler.listen(this.select_box_, 'change', this.update);
   handler.listen(this.key_handler_, 'key', this.handleKey_);
+  handler.listen(el, 'click', this.handleClick_);
+  handler.listen(el, 'blur', this.close_);
 };
 
 
 ian.ui.Select.prototype.exitDocument = function () {
   goog.base(this, 'exitDocument');
 
+  var el = this.getElement();
   var handler = this.getHandler();
+
   handler.unlisten(this.select_box_, 'change', this.update);
   handler.unlisten(this.key_handler_, 'key', this.handleKey_);
+  handler.unlisten(el, 'click', this.handleClick_);
+  handler.unlisten(el, 'blur', this.close_);
 
   this.key_handler_.dispose();
   this.key_handler_ = null;
@@ -92,8 +134,18 @@ ian.ui.Select.prototype.update = function () {
   var option_label = dom.getTextContent(option) || option.value;
   var value = option.value || option_label;
 
+  var old_dropdown_item = this.dropdown_items_[this.selected_index_ - 1];
+  var new_dropdown_item = this.dropdown_items_[selected_index - 1];
+  if (old_dropdown_item) {
+    goog.dom.classes.remove(old_dropdown_item, 'selected');
+  }
+  if (new_dropdown_item) {
+    goog.dom.classes.add(new_dropdown_item, 'selected');
+  }
+
   dom.setTextContent(this.value_el_, option_label);
 
+  this.selected_index_ = selected_index;
   if (value !== this.value_) {
     this.value_ = value;
 
@@ -103,18 +155,63 @@ ian.ui.Select.prototype.update = function () {
 };
 
 
+ian.ui.Select.prototype.open_ = function () {
+  goog.style.setElementShown(this.dropdown_el_, true);
+  this.is_open_ = true;
+};
+
+
+ian.ui.Select.prototype.close_ = function () {
+  goog.style.setElementShown(this.dropdown_el_, false);
+  this.is_open_ = false;
+};
+
+
 ian.ui.Select.prototype.handleKey_ = function (e) {
+  if (e.keyCode === goog.events.KeyCodes.DOWN && !this.is_open_) {
+    this.open_();
+    return;
+  }
+
   switch (e.keyCode) {
   case goog.events.KeyCodes.UP:
-  case goog.events.KeyCodes.DOWN:
   case goog.events.KeyCodes.LEFT:
-  case goog.events.KeyCodes.RIGHT:
-  case goog.events.KeyCodes.HOME:
-  case goog.events.KeyCodes.END:
-  case goog.events.KeyCodes.PAGE_UP:
-  case goog.events.KeyCodes.PAGE_DOWN:
-    this.update();
+    this.select_box_.selectedIndex -= 1;
     break;
+  case goog.events.KeyCodes.DOWN:
+  case goog.events.KeyCodes.RIGHT:
+    this.select_box_.selectedIndex += 1;
+    break;
+  case goog.events.KeyCodes.HOME:
+  case goog.events.KeyCodes.PAGE_UP:
+    this.select_box_.selectedIndex = 0;
+    break;
+  case goog.events.KeyCodes.END:
+  case goog.events.KeyCodes.PAGE_DOWN:
+    this.select_box_.selectedIndex = this.select_box_.options.length;
+    break;
+  default:
+    return;
+  }
+
+  this.update();
+};
+
+
+ian.ui.Select.prototype.handleClick_ = function (e) {
+  if (!this.is_open_) {
+    this.open_();
+    this.getElement().focus();
+  } else {
+    var target = e.target;
+    if (target.tagName === 'LI') {
+      var index = goog.array.indexOf(this.dropdown_items_, target);
+      if (index !== -1) {
+        this.select_box_.selectedIndex = index + 1;
+        this.update();
+      }
+    }
+    this.close_();
   }
 };
 
