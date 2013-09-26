4aa0fec8af07b1cfd9b75f1e25d867ccec177770
jankuca <jan@jankuca.com>
2013-09-25 23:44:54 -0700

    add ian.string.toSnakeCase(str)

--- js/string.js
+++ js/string.js
@@ -14,3 +14,15 @@ ian.string.toPascalCase = function (str) {
   });
   return pascal;
 };
+
+
+/**
+ * Converts a string from selector-case to snake_case (e.g. my-unicorn-poop to
+ *   my_unicorn_poop).
+ * @param {string} str The string in selector-case form.
+ * @return {string} The string in PascalCase form.
+ */
+ian.string.toSnakeCase = function (str) {
+  var snake = str.replace(/-/g, '_');
+  return snake;
+};
