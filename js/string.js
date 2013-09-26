afeffd2be9fb398adf5b9cf4dfa5cb460feca911
jankuca <jan@jankuca.com>
2013-09-25 22:42:41 -0700

    add ian.string.toPascalCase(str)

---
+++ js/string.js
@@ -0,0 +1,16 @@
+goog.provide('ian.string');
+
+
+/**
+ * Converts a string from selector-case to PascalCase (e.g. my-unicorn-poop to
+ *   MyUnicornPoop).
+ * @param {string} str The string in selector-case form.
+ * @return {string} The string in PascalCase form.
+ */
+ian.string.toPascalCase = function (str) {
+  var camel = goog.string.toCamelCase(str);
+  var pascal = camel.replace(/^[a-z]/, function (letter) {
+    return letter.toUpperCase();
+  });
+  return pascal;
+};
