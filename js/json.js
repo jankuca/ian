b1d623664c273ee9217358f359556fba97f24063
jankuca <jan@jankuca.com>
2013-10-10 15:03:16 +0200

    improve ian.json

--- js/json.js
+++ js/json.js
@@ -1,5 +1,8 @@
 goog.provide('ian.json');
 
+goog.require('goog.array');
+goog.require('ian.path');
+
 
 /**
  * Stringifies a value into possibly invalid JSON (but a valid JS expression)
@@ -10,55 +13,108 @@ goog.provide('ian.json');
  *   stringifyWithKnownPaths({ a: 1, b: 2, c: { d: 3, e: 4 }}, [ 'a', 'c.d' ])
  *   <- '{ a: 1, "b": 2, c: { d: 3, "e": 4 }}'
  *
- * @param {*} value The value to serialize.
- * @param {!Array.<string>} paths A list of paths to exclude from quoting.
- * @param {number=} spaces The number of spaces to use for indentation.
- *   Defaults to 0 (i.e. "do not pretty-print").
- * @param {(function(*):*)=} resolver A function that can transform any value.
- * @return {string} A serialization of the value. Valid JS expression.
- */
-ian.json.stringifyWithKnownPaths = function (value, paths, spaces, resolve) {
-  paths = ian.path.getAllPaths(paths, '.');
-
-  return ian.json.stringifyWithKnownPaths_(value, paths, spaces, resolve);
-};
-
-
-/**
+ * You can also use "*" as a wildcard identifier that matches any key but does
+ * not exclude the key from being quoted.
+ *   stringifyWithKnownPaths({ a: { b: { c: 1 }}}, [ '*.b' ])
+ *   <- '{ "a": { b: { "c": 1 }}}'
+ *
  * @param {*} val The value to serialize.
  * @param {!Array.<string>} paths A list of paths to exclude from quoting.
  * @param {number=} spaces The number of spaces to use for indentation.
  *   Defaults to 0 (i.e. "do not pretty-print").
  * @param {(function(*):*)=} resolver A function that can transform any value.
+ * @return {string} A serialization of the value. Valid JS expression.
+ */
+ian.json.stringifyWithKnownPaths = function (val, paths, spaces, resolve) {
+  paths = ian.path.getAllPaths(paths, '.');
+
+  return ian.json.stringifyWithKnownPaths_(val, paths, spaces, resolve);
+};
+
+
+/**
+ * @param {*} value The value to serialize.
+ * @param {!Array.<string>} paths A list of paths to exclude from quoting.
+ * @param {number=} spaces The number of spaces to use for indentation.
+ *   Defaults to 0 (i.e. "do not pretty-print").
+ * @param {(function(*):*)=} resolver A function that can transform any value.
  * @param {string} base_path The base path to prepend in front of every key.
+ * @param {number} depth The tree depth or the indentation level.
  * @return {string} A serialization of the value. Valid JS expression.
  */
 ian.json.stringifyWithKnownPaths_ =
-    function (val, paths, spaces, resolve, base_path) {
+    function (value, paths, spaces, resolve, base_path, depth) {
+  base_path = base_path || '';
+  depth = depth || 0;
+
   switch (typeof value) {
+  case 'undefined':
+    return 'null';
+
   case 'boolean':
   case 'number':
   case 'string':
     return JSON.stringify(resolve ? resolve(value) : value);
 
   case 'object':
-    var json = '{';
+    if (value === null) {
+      return 'null';
+    }
+
+    var json = '';
+
+    if (goog.isArray(value)) {
+      json += '[';
+
+      for (var i = 0, ii = value.length; i < ii; ++i) {
+        if (spaces > 0) {
+          json += '\n' + ian.json.generateIndentation_((depth + 1) * spaces);
+        }
+        json += ian.json.stringifyWithKnownPaths_(
+            value[i], paths, spaces, resolve, path, depth + 1);
+        json += ',';
+      }
+
+      json = json.replace(/,$/, '');
+      json += '\n' + ian.json.generateIndentation_(depth * spaces) + ']';
+
+      return json;
+    }
+
+    json += '{';
 
     for (var key in value) {
       var path = base_path + '.' + key;
       if (path[0] === '.') path = path.substr(1);
 
-      if (paths.indexOf(path) !== -1) {
+      json += '\n' + ian.json.generateIndentation_((depth + 1) * spaces);
+
+      if (ian.path.testAgainstSome(paths, path)) {
         json += key;
       } else {
         json += '"' + key.replace(/"/, '\g"') + '"';
       }
 
+      json += ':' + (spaces > 0 ? ' ' : '');
       json += ian.json.stringifyWithKnownPaths_(
-          value, paths, spaces, resolve, path);
+          value[key], paths, spaces, resolve, path, depth + 1);
+
+      json += ',';
     }
 
-    json += '}';
+    json = json.replace(/,$/, '');
+    json += '\n' + ian.json.generateIndentation_(depth * spaces) + '}';
+
     return json;
   }
+};
+
+
+ian.json.generateIndentation_ = function (spaces) {
+  var indentation = '';
+  while (spaces--) {
+    indentation += ' ';
+  }
+
+  return indentation;
 };
--- js/path.js
+++ js/path.js
@@ -1,5 +1,7 @@
 goog.provide('ian.path');
 
+goog.require('goog.array');
+
 
 /**
  * Takes a list of paths and returns a list that contains all the original
@@ -20,11 +22,49 @@ ian.path.getAllPaths = function (paths, sep) {
     var path = '';
     for (var o = 0, oo = levels.length; o < oo; ++o) {
       path += '.' + levels[o];
-      if (result.indexOf(path.substr(1)) === -1) {
+      if (levels[o] !== '*' && levels[o] !== '**' &&
+          result.indexOf(path.substr(1)) === -1) {
         result.push(path.substr(1));
       }
     }
   }
 
   return result;
+};
+
+
+/**
+ * Checks whether a path matches a pattern that can contain "*" and "**"
+ * wildcards to represent one or more levels respectively.
+ * @param {string} pattern The pattern against which to match.
+ * @param {string} path The path to match against the pattern.
+ * @param {string=} separator The level separator. Defaults to a slash ("/").
+ * @return {boolean} Whether the path matches the pattern.
+ */
+ian.path.test = function (pattern, path, separator) {
+  separator = (separator || '/').replace(/\//g, '\\/');
+
+  pattern = pattern.replace(/\*\*/g, '.*');
+  pattern = pattern.replace(/\*/g, '[^' + separator + ']*?');
+
+  var rx = new RegExp('^' + pattern + '$');
+  return rx.test(path);
+};
+
+
+/**
+ * Checks whether a path matches a pattern from the provided array of patterns.
+ * A single match is enough for a positive result. The patterns can contain
+ * "*" and "**" wildcards to represent one or more levels respectively.
+ * @param {string} patterns The pattern against which to match.
+ * @param {string} path The path to match against the patterns.
+ * @param {string=} separator The level separator. Defaults to a slash ("/").
+ * @return {boolean} Whether the path matches any of the patterns.
+ */
+ian.path.testAgainstSome = function (patterns, path, separator) {
+  if (patterns.length === 0) return false;
+
+  return goog.array.some(patterns, function (pattern) {
+    return ian.path.test(pattern, path, separator);
+  });
 };
