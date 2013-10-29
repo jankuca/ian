f4674de751602cad0c1ebd0bae0248d2c3bb2774
Matouš Skála <skala.matous@gmail.com>
2013-10-29 17:01:29 +0100

    add component state class auto update

--- js/ui/compiler.js
+++ js/ui/compiler.js
@@ -171,6 +171,7 @@ ian.ui.Compiler.prototype.compileElement_ = function (element) {
 
     if (component_class_name) {
       var state = this.getStateFromClasses(component_class_name, class_names);
+      component.setName(component_class_name);
       component.setState(state);
     }
   }
