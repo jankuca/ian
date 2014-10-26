827f962f124b11d38ba1478267b08784a29ead6e
jankuca <jan@jankuca.com>
2014-10-26 13:03:35 +0100

    add ian.mobile.Controller#createView()

--- js/mobile/controller.js
+++ js/mobile/controller.js
@@ -12,7 +12,7 @@ goog.require('ian.Controller');
 ian.mobile.Controller = function (router, cfg) {
   ian.Controller.call(this, router, cfg);
 
-  this.view = null;
+  this.view_ = null;
 };
 
 goog.inherits(ian.mobile.Controller, ian.Controller);
@@ -22,7 +22,7 @@ goog.inherits(ian.mobile.Controller, ian.Controller);
  * @param {ian.mobile.View} view The view to associate with the controller.
  */
 ian.mobile.Controller.prototype.setView = function (view) {
-  this.view = view;
+  this.view_ = view;
 };
 
 
@@ -30,5 +30,15 @@ ian.mobile.Controller.prototype.setView = function (view) {
  * @return {ian.mobile.View} The view associated with the controller.
  */
 ian.mobile.Controller.prototype.getView = function () {
-  return this.view || null;
+  if (!this.view_) {
+    this.view_ = this.createView();
+  }
+  return this.view_;
+};
+
+
+ian.mobile.Controller.prototype.createView = function () {
+  // override this
+  var view = new ian.mobile.View();
+  return view;
 };
